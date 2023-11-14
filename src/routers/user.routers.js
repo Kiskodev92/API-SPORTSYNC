836c1ab93d8d577ac1be45cd51e57c283deb6dc9
Kiskotkd <fjgp92@hotmail.com>
2023-11-14 10:26:21 +0100

    correccion conexion

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 const router = Router();
-const userCtrl = require ("../controller/usuario.controller");
+const userCtrl = require ("../controller/user.controller");
 
 router.post("/register", userCtrl.postRegister);
 router.post("/login", userCtrl.postLogin);
