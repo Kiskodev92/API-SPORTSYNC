b31371f1cf3cf54f498e783dacf1478bf7e560f9
Kiskotkd <fjgp92@hotmail.com>
2023-11-15 10:33:44 +0100

    endpoint evento y postman ok

--- src/controller/event.controller.js
+++ src/controller/event.controller.js
@@ -7,15 +7,14 @@ const postAddEvent = async (req, res) => {
 try 
 {
       
-    let sql = "INSERT INTO eventos (id_usuario, titulo, fecha, hora, descripcion, foto)" + 
-                                    "VALUES ('" +  req.body.id_usuario + "', '" +
-                                                req.body.titulo + "', '" +
-                                                req.body.fecha + "', '" +
-                                                req.body.hora + "', '" +
-                                                req.body.descripcion + "', '" +
-                                                req.body.foto + "')";
-
-
+  let sql = "INSERT INTO eventos (id_usuario, desporte_id, titulo, fecha, hora, descripcion, foto)" + 
+  "VALUES ('" +  req.body.id_usuario + "', '" +
+              req.desporte_id + "', '" +
+              req.body.titulo + "', '" +
+              req.body.fecha + "', '" +
+              req.body.hora + "', '" +
+              req.body.descripcion + "', '" +
+              req.body.foto + "')";
   
     console.log(sql);
     let [result] = await pool.query(sql);
