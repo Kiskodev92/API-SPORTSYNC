b6dfef7bedfb6133d2e4e4d161c15d18d95bb6bc
borja <borjaswim1995@gmail.com>
2023-11-17 17:45:54 +0100

    nada

--- src/controller/event.controller.js
+++ src/controller/event.controller.js
@@ -7,14 +7,15 @@ const postAddEvent = async (req, res) => {
 try 
 {
       
-  let sql = "INSERT INTO eventos (id_usuario, desporte_id, titulo, fecha, hora, descripcion, foto)" + 
-  "VALUES ('" +  req.body.id_usuario + "', '" +
-              req.body.desporte_id + "', '" +
-              req.body.titulo + "', '" +
-              req.body.fecha + "', '" +
-              req.body.hora + "', '" +
-              req.body.descripcion + "', '" +
-              req.body.foto + "')";
+  let sql = "INSERT INTO eventos (id_usuario, id_deporte,titulo, fecha,  descripcion, foto)" + 
+                                    "VALUES ('" +  req.body.id_usuario + "', '" +
+                                                req.body.id_deporte + "', '" +
+                                                req.body.titulo + "', '" +
+                                                req.body.fecha + "', '" +
+                                                req.body.descripcion + "', '" +
+                                                req.body.foto + "')";
+
+
   
     console.log(sql);
     let [result] = await pool.query(sql);
