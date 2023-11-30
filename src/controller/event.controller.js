66441fdae61899add25041ada79f2981bdd4d4ee
borja <borjswim1995@gmail.com>
2023-11-30 16:22:24 +0100

    nada

--- src/controller/event.controller.js
+++ src/controller/event.controller.js
@@ -25,7 +25,7 @@ const postAddEvent = async (req, res) => {
 
 try 
 {
-      console.log(req.body);
+  console.log(req.body);
   let sql = "INSERT INTO eventos (id_usuario, id_deporte, titulo, fecha, descripcion, foto)" + 
   "VALUES ('" +  req.body.id_usuario + "', '" +
               req.body.id_deporte + "', '" +
@@ -48,6 +48,7 @@ catch(err)
         {
           console.log(err);
         }
+
 }
 
 // MOSTRAT EVENTOS EN HOME
@@ -75,13 +76,12 @@ const getEvent = async (req, res) => {
 const getOne = async (req, res) => {
   
   try{
-    
-    let valor = [req.query.titulo]
 
-    let sql = 'SELECT * FROM eventos WHERE titulo = ?'
-  
-      console.log('evento no encontrado');
-    
+    if(req.query.provincia ){
+
+    let valor = [req.query.provincia]
+
+    let sql = 'SELECT * FROM eventos WHERE provincia = ?'
 
     console.log(sql);
     let [result] = await pool.query(sql,valor)
@@ -91,6 +91,35 @@ const getOne = async (req, res) => {
     }else{
       res.send({error:false, codigo:200, mensaje:'evento encontrado', data:result})
     }
+
+  }
+  
+  }catch(err){
+    console.log(err);
+  }
+}
+
+const getOne2 = async (req, res) => {
+  
+  try{
+
+    if(req.query.titulo ){
+
+    let valor = [req.query.titulo]
+
+    let sql = 'SELECT * FROM eventos WHERE titulo = ?'
+
+    console.log(sql);
+    let [result] = await pool.query(sql,valor)
+    console.log(result);
+    if(result.length == 0){
+      res.send({error:true, codigo:404, mensaje:'evento no encontrado'})
+    }else{
+      res.send({error:false, codigo:200, mensaje:'evento encontrado', data:result})
+    }
+
+  }
+  
   }catch(err){
     console.log(err);
   }
@@ -144,5 +173,25 @@ const getDeportUs = async (req,res) =>{
 
 }
 
+const deleteevent = async (req, res) =>
+{
+    try
+    {
+        console.log(req.body);
 
-module.exports = {postAddEvent, getEvent, getOne, getDeporte, postBoton,getDeportUs};
+        let params = [req.query.id_usuario,req.query.id_evento]
+        let sql = "DELETE FROM usEvent WHERE id_usuario = ? AND id_evento = ?"
+        console.log(sql);
+        let [result] = await pool.query(sql,params)
+        res.send(result)
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+
+}
+
+
+
+module.exports = {postAddEvent, getEvent, getOne, getDeporte, postBoton,getDeportUs,deleteevent,getOne2};
